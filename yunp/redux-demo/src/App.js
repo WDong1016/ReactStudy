5cae8421fe5d587a280172fc8dd0d9bf42a8575a
WDong <wdong@WilliamdeMacBook-Pro.local>
2020-10-23 23:21:01 +0800

    Redux NOT react-redux

--- yunp/redux-demo/src/App.js
+++ yunp/redux-demo/src/App.js
@@ -3,8 +3,12 @@ import React, { Component } from 'react'
 export default class App extends Component {
   render() {
     return (
-      <div>
-        Hello
+      <div className="container">
+        <h1 className="jumbotron-heading text-center">{this.props.numValue}</h1>
+        <p className="text-center">
+          <button onClick={this.props.onIncrement} className="btn btn-success">Increment</button>
+          <button onClick={this.props.onDecrement} className="btn btn-danger">Decrement</button>
+        </p>
       </div>
     )
   }
--- yunp/redux-demo/src/index.js
+++ yunp/redux-demo/src/index.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import './App.css'
 
+import { createStore} from 'redux'
+import counter from './reducers/counter'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
+const store = createStore(counter)
+store.subscribe(()=>console.log(store.getState()));
+const render = () =>{
+  ReactDOM.render(
+    <React.StrictMode>
+      <App 
+        onIncrement = {()=>store.dispatch({type: "INCREMENT"})}
+        onDecrement = {()=>store.dispatch({type: "DECREMENT"})}
+        numValue = {store.getState()}
+      />
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
+}
+render();
+store.subscribe(render)
